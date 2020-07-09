67ad922546062de182ab90f104fb2dfc8aab3a85
kylejohnson-37 <68037716+kylejohnson-37@users.noreply.github.com>
2020-07-09 09:43:24 -0400

    improve ui add npm files

---
+++ start.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = new express();
+
+app.get('/', function(request, response){
+    response.sendFile(__dirname + '/Index.html');
+});
+
+app.listen(8040, () => {
+    console.log('Example app listening on port 8000!')
+  });
